facabc2d899e7661f9de01dfbeca485e65f6e589
Piyaporn14 <fernpiyaporn14@gmail.com>
2021-04-23 15:34:52 +0700

    add LUDecompose

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -43,8 +43,8 @@ const Topbar = () => {
                         <DropdownItem href="/GaussElimination" >Gauss Elimination</DropdownItem>
                         <DropdownItem href="/GaussJordan " >Gauss-Jordan Elimination</DropdownItem>
                         <DropdownItem href="/ConjugateGradient" >Conjugate Gradient</DropdownItem>
-                        <DropdownItem href="/Cholesky" >Cholesky Decomposition</DropdownItem>
-                        <DropdownItem href="/Bisection" >LU Decomposition</DropdownItem>
+                        <DropdownItem href="/Cholesky" >Cholesky</DropdownItem>
+                        <DropdownItem href="/LUDecompose" >LUDecompose</DropdownItem>
                         <DropdownItem href="/Bisection" >Gauss-Seidel</DropdownItem>
                         <DropdownItem href="/Bisection" >Jacobi's Method</DropdownItem>
                     </DropdownMenu>
